6304edea8fd621a9a49f9b25b580d89bfad67e4a
Marion Geary Bauman <gearymf@gmail.com>
2024-04-17 01:28:55 -0400

    Save all models, update poster

--- code/eda/trip-viz.R
+++ code/eda/trip-viz.R
@@ -24,3 +24,31 @@ trips |>
 
 # Save plot
 ggsave("code/poster/trip-purpose-distribution.png", width = 8, height = 5, dpi = 300)
+
+all_data <- read_csv("models/all_probs.csv")
+
+all_data |>
+    select(NN, `Logistic Regression`, Xgboost, SVM, Actual) |>
+    group_by(Actual) |>
+    mutate(count = n()) |>
+    ungroup() |>
+    pivot_longer(cols = -c(Actual, count), names_to = "Model", values_to = "Prediction") |>
+    filter(Actual == Prediction) |>
+    summarise(.by = c(Model, Actual, Prediction), n = n(), count = first(count)) |>
+    mutate(perc = n/count) |>
+    ggplot(aes(x = Actual, y = perc, color = Model)) +
+    geom_point(aes(shape = Model), size = 3) +
+    geom_line() +
+    scale_y_continuous(labels = scales::percent) +
+    labs(
+        title = "Model Performance by Class",
+        x = "Trip Purpose (True)",
+        y = "Class Prediction Accuracy"
+    ) +
+    #'Home-based other (HBO)', 'Home-based shopping (HBSHP)','Home-based social/recreational (HBSOC)', 'Home-based work (HBW)','Not a home-based trip (NHB)'],
+    scale_x_continuous(labels = c('Home-based\nother (HBO)', 'Home-based \nshopping (HBSHP)','Home-based social/\nrecreational (HBSOC)', 'Home-based work (HBW)', 'Not a home-based\ntrip (NHB)')) +
+    theme_minimal() +
+    scale_color_manual(values = c("#002e62", "#BBBCBC", "#5FA343", "#862633"))
+
+# Save plot
+ggsave("code/poster/model-performance-by-class.png", width = 8, height = 5, dpi = 300)
