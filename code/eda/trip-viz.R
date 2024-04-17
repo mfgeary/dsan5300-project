0705b21e50423d3b469d73f2f6d9418672546cc7
Marion Geary Bauman <gearymf@gmail.com>
2024-04-17 09:50:43 -0400

    Finalize all models and predictions, add visuals

--- code/eda/trip-viz.R
+++ code/eda/trip-viz.R
@@ -28,7 +28,7 @@ ggsave("code/poster/trip-purpose-distribution.png", width = 8, height = 5, dpi =
 all_data <- read_csv("models/all_probs.csv")
 
 all_data |>
-    select(NN, `Logistic Regression`, Xgboost, SVM, Actual) |>
+    select(NN, `Logistic Regression`, Xgboost, SVM, RF, Actual) |>
     group_by(Actual) |>
     mutate(count = n()) |>
     ungroup() |>
@@ -48,7 +48,29 @@ all_data |>
     #'Home-based other (HBO)', 'Home-based shopping (HBSHP)','Home-based social/recreational (HBSOC)', 'Home-based work (HBW)','Not a home-based trip (NHB)'],
     scale_x_continuous(labels = c('Home-based\nother (HBO)', 'Home-based \nshopping (HBSHP)','Home-based social/\nrecreational (HBSOC)', 'Home-based work (HBW)', 'Not a home-based\ntrip (NHB)')) +
     theme_minimal() +
-    scale_color_manual(values = c("#002e62", "#BBBCBC", "#5FA343", "#862633"))
+    scale_color_manual(values = c("#002e62", "#BBBCBC", "#5FA343", "#862633", "#F2A900"))
 
 # Save plot
 ggsave("code/poster/model-performance-by-class.png", width = 8, height = 5, dpi = 300)
+
+# Visualize the certainty of the model
+all_data |>
+    select(-c(NN, `Logistic Regression`, Xgboost, SVM, RF)) |>
+    pivot_longer(cols = -c(Actual), names_to = "Model", values_to = "Prediction") |>
+    filter(Actual == as.numeric(str_split_fixed(Model, "_", 2)[, 2])) |>
+    mutate(model = str_split_fixed(Model, "_", 2)[, 1]) |>
+    mutate(Actual = as.character(Actual)) |>
+    ggplot(aes(x = Actual, y = Prediction, color = model, group = Model)) +
+    geom_boxplot(outliers = FALSE) +
+    theme_minimal() +
+    scale_color_manual(values = c("#002e62", "#BBBCBC", "#5FA343", "#862633", "#F2A900")) +
+    scale_x_discrete(labels = c('Home-based\nother (HBO)', 'Home-based \nshopping (HBSHP)','Home-based social/\nrecreational (HBSOC)', 'Home-based work (HBW)', 'Not a home-based\ntrip (NHB)')) +
+    labs(
+        title = "Model Prediction Certainty by Class",
+        x = "Trip Purpose (True)",
+        y = "Model Prediction Certainty"
+    )
+
+# Save plot
+ggsave("code/poster/model-prediction-certainty-by-class.png", width = 8, height = 5, dpi = 300)
+
